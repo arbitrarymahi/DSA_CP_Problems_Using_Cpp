e1b126f01910cbd0197fe9a94f4cce601ff9d03a
arbitrarymahi <iamaanjna@gmail.com>
2021-12-28 17:49:31 +0530

    min steps to travers pairs  in given order in grid

---
+++ min_steps_in_infinite_grid.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int coverPoints(vector<int> &A, vector<int> &B) {
+    int currentX = A[0];
+    int currentY = B[0];
+    int targetx = 1;
+    int targety = 1;
+    int steps = 0;
+    int n = 0;
+    while(n!=A.size()-1){
+        // cout<<n<< " " << steps<< " i "<< currentX <<" " <<currentY<<endl;
+        
+        if( (currentX-1 == A[targetx] && currentY-1 == B[targetx]  ) 
+         || (currentX-1 == A[targetx] && currentY == B[targetx]  ) 
+         || (currentX-1 == A[targetx] && currentY+1 == B[targetx]  ) 
+         || (currentX == A[targetx] && currentY-1 == B[targetx]  ) 
+         || (currentX == A[targetx] && currentY+1 == B[targetx]  ) 
+         || (currentX+1 == A[targetx] && currentY-1 == B[targetx]  ) 
+         || (currentX+1 == A[targetx] && currentY == B[targetx]  ) 
+         || (currentX+1 == A[targetx] && currentY+1 == B[targetx]  )  )
+         {
+            //  cout<<"tru"<<endl;
+            currentX = A[targetx++];
+            currentY = B[targety++];
+            steps++;
+            n++;
+        
+        }
+        else{
+            
+            // fin dist between pairs and proceed..
+            int x = abs(A[targetx]-currentX);
+            int y = abs(B[targety]-currentY);
+            steps+= max(x,y);
+            currentX = A[targetx++];
+            currentY = B[targety++];
+            n++;
+           
+        }
+        // steps++;
+    }
+    return steps;
+
+    }
+int main()
+{
+    vector<int> a = { -7, -13 };
+    vector<int> b = { 1, -5,};
+    // vector<int> a = { -4,   1, -4, 8, -11, -12, -13, -3, -4, -4, -14, 7, -2, -2, -5, 5,   -1, 0};
+    // vector<int> b = { -8, -15, -4, 3,   11, 8,   -15, 4, 1,    7, 3,  9, -9, -9, -13, 10,-14, -8 };
+    int ans = coverPoints(a,b);
+    cout<<ans<<endl;
+    return 0;
+}
